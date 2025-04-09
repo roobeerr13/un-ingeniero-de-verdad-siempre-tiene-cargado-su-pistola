7634948531769dc9f2a92dec77b55135a84729ff
Roberto Jimenez <robertojj2006@gmail.com>
2025-04-09 09:48:58 +0200

    imp readme link

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # un-ingeniero-de-verdad-siempre-tiene-cargado-su-pistola
+https://github.com/roobeerr13/un-ingeniero-de-verdad-siempre-tiene-cargado-su-pistola.git
---
+++ caballo/caballos.py
@@ -0,0 +1,7 @@
+class Caballos:
+    def __init__(self, origen, destino):
+        self.origen = origen
+        self.destino = destino
+
+    def __repr__(self):
+        return f"Caballo(origen={self.origen}, destino={self.destino})"
